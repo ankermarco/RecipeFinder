6b5136a67095254dd4a5af6f0dcc55d94f8074f2
Ke Ma <kema2012@gmail.com>
2017-08-09 13:06:17 +0100

    Add RecipesViewController and connect with storyboard

---
+++ RecipeFinder/RecipeFinder/Controllers/RecipesViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RecipesViewController.swift
+//  RecipeFinder
+//
+//  Created by Ke Ma on 09/08/2017.
+//  Copyright © 2017 ke.ma. All rights reserved.
+//
+
+import UIKit
+
+class RecipesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
