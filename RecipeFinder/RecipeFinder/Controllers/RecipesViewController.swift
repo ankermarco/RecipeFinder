bb8041ac4e2ce861e9f506c594134f20ab134a91
Ke Ma <kema2012@gmail.com>
2017-08-09 21:30:48 +0100

    Connect RecipesViewController with RecipesInteractor

--- RecipeFinder/RecipeFinder/Controllers/RecipesViewController.swift
+++ RecipeFinder/RecipeFinder/Controllers/RecipesViewController.swift
@@ -8,17 +8,66 @@
 
 import UIKit
 
-class RecipesViewController: UIViewController {
+class RecipesViewController: UIViewController
+{
+  @IBOutlet weak var searchBar: UISearchBar!
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  var recipesInteractor : RecipeInteractor!
+  var recipes = [Recipe]()
+  
+  fileprivate let CellId = "CellID"
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    searchBar.delegate = self
+    tableView.dataSource = self
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+}
 
-        // Do any additional setup after loading the view.
+extension RecipesViewController: UISearchBarDelegate{
+  
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    let requestUrl = "http://www.recipepuppy.com/api/?q=\(searchText)"
+    guard searchText.characters.count > 0 else {
+      recipes.removeAll()
+      self.tableView.reloadData()
+      return
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    if recipesInteractor == nil {
+      recipesInteractor = RecipeInteractor(requestUrl)
+    } else {
+      recipesInteractor.requestUrl = requestUrl
     }
+    recipes = recipesInteractor.fetchRecipes()
+    self.tableView.reloadData()
+  }
+}
 
+extension RecipesViewController: UITableViewDataSource
+{
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return recipes.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: CellId, for: indexPath)
+    cell.textLabel?.text = recipes[indexPath.row].name
+    
+    return cell
+  }
 }
